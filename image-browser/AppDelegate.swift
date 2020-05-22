539c7fc89ad098bb8ff9f0cb1824e8f95657b2f6
Shivam Jaiswal <shivam.jaiswal@lalamove.com>
2020-05-22 07:37:11 +0530

    initial setup

---
+++ image-browser/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  image-browser
+//
+//  Created by Shivam Jaiswal on 22/05/20.
+//  Copyright © 2020 Shivam Jaiswal. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
---
+++ image-browser/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  image-browser
+//
+//  Created by Shivam Jaiswal on 22/05/20.
+//  Copyright © 2020 Shivam Jaiswal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
